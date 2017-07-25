60adf23fe5c34350acab03d3746731e315283674
richan24 <30393267+richan24@users.noreply.github.com>
2017-07-25 23:09:41 +0530

    adding context menu

--- app/src/main/java/com/trackerbuddy/carassist/MainActivity.java
+++ app/src/main/java/com/trackerbuddy/carassist/MainActivity.java
@@ -1,15 +1,27 @@
 package com.trackerbuddy.carassist;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+     public Map<Integer,BluetoothDevice> BluetoothMenu =  new HashMap<Integer, BluetoothDevice>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +30,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnCreateContextMenuListener(this);
+        //registerForContextMenu(fab);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+              //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+              //          .setAction("Action", null).show();
+                registerForContextMenu(view);
             }
-        });
+        });*/
     }
 
     @Override
@@ -49,4 +64,39 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        if(v.getId()== R.id.fab) {
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            menu.setHeaderTitle("Select Car Device");
+            int menuId= 1;
+
+            if (pairedDevices.size() > 0) {
+                // There are paired devices. Get the name and address of each paired device.
+                for (BluetoothDevice device : pairedDevices) {
+                    String deviceName = device.getName();
+                    BluetoothMenu.put(menuId,device);
+                    menu.add(0,menuId,0,deviceName);
+                    menuId++;
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        int menuId=item.getItemId();
+        BluetoothDevice cardevice = BluetoothMenu.get(menuId);
+        Toast.makeText(this, "Device "+cardevice.getName()+" is set!",Toast.LENGTH_LONG).show();
+        return true;
+      /*  Snackbar.make(this.findViewById(R.id.fab), "Device"+cardevice.getName()+" is set!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        Toast.makeText(closeContextMenu(),"Your car device is set!",3000);
+        //store this device
+        */
+    }
+
 }
